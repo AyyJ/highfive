8e445f023ddf2ae6fa3a77b87792b898e39f507f
DanielCastleman <dcastlem@ucsd.edu>
2017-04-24 20:22:28 -0700

    Valid & Invalid Number

--- test/test.js
+++ test/test.js
@@ -2,21 +2,55 @@ var assert = require('assert');
 var index = require('../functions/index.js');  // our module
 
 describe("Valid Phone and Format Phone", function() {
-  describe("phone number is valid", function() {
+  describe("Check if Valid Phone Number", function() {
 
-    it("says it's valid", function() {
+    it("Valid Phone Number", function() {
         var phoneNumber = "1231231230";
         var response = index.test_validatePhone(phoneNumber);
 
         assert.equal(response, true);
     });
 
-  	it("Invalid formatted phone #", function() {
+    it("Valid Phone Number", function() {
+        var phoneNumber = "(123)456-7890";
+        var response = index.test_validatePhone(phoneNumber);
 
+        assert.equal(response, true);
     });
 
-    it("Check against nonsense", function() {
+    it("Valid Phone Number", function() {
+        var phoneNumber = "(123)4567890";
+        var response = index.test_validatePhone(phoneNumber);
 
+        assert.equal(response, true);
+    });
+
+    it("Valid Phone Number", function() {
+        var phoneNumber = "123456-7890";
+        var response = index.test_validatePhone(phoneNumber);
+
+        assert.equal(response, true);
+    });
+
+    it("Weird Valid Phone Number", function() {
+        var phoneNumber = "123-456-7890";
+        var response = index.test_validatePhone(phoneNumber);
+
+        assert.equal(response, true);
+    });
+
+  	it("Invalid Phone Number", function() {
+        var phoneNumber = "123123123";
+        var response = index.test_validatePhone(phoneNumber);
+
+        assert.equal(response, false);
+    });
+
+    it("Invalid Phone Number", function() {
+        var phoneNumber = "12312e123";
+        var response = index.test_validatePhone(phoneNumber);
+
+        assert.equal(response, false);
     });
   });
 
