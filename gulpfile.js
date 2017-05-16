2f15a8d786d5ef9e7914aaed516e88520229217c
Arthur de Jesus <adejesus@ucsd.edu>
2017-05-16 15:16:45 -0700

    HOTFIX: added istanbul + homepage fixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,50 @@
 var gulp = require('gulp'),
-   jshint = require('gulp-jshint'),
-   jsdoc = require('gulp-jsdoc3'),
-   mocha = require('gulp-mocha');
+jshint = require('gulp-jshint'),
+jsdoc = require('gulp-jsdoc3'),
+mocha = require('gulp-mocha');
+instanbul = require('gulp-instanbul')
 
 /**
 * Lint Checker
 */
 gulp.task('lint', function () {
-   gulp.src('./**/*.js')
-      .pipe(jshint())
+  gulp.src('./**/*.js')
+  .pipe(jshint())
 })
 
 /**
-* Run Mocha Tests
+* Instanbul + Mocha tests
 */
-gulp.task('mocha', () =>
-   gulp.src('test/test.js', {read: false})
-      .pipe(mocha({reporter: 'spec'}))
-);
+gulp.task('test', function () {
+  return gulp.src('./server/routes/*.js')
+  // Right there
+  .pipe(istanbul({includeUntested: true}))
+  .on('finish', function () {
+    gulp.src('./server/test/test.js')
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports({
+      dir: './public/coverage',
+      reporters: [ 'lcov' ],
+      reportOpts: { dir: './public/coverage'}
+    }));
+  });
+});
+
 /**
 * Overwrite fresh-installed jsdocConfig
 */
 gulp.task('overwrite', () =>
 gulp.src('./jsdocConfig.json')
-  .pipe(gulp.dest('./node_modules/gulp-jsdoc3/dist'))
-  .pipe(gulp.dest('./node_modules/gulp-jsdoc3/src'))
+.pipe(gulp.dest('./node_modules/gulp-jsdoc3/dist'))
+.pipe(gulp.dest('./node_modules/gulp-jsdoc3/src'))
 );
 /**
 * Run documentation generator
 */
 gulp.task('jsdoc', function (cb) {
-    var config = require('./jsdocConfig.json')
-    gulp.src(['README.md', './routes/*.js'], {read: false})
-        .pipe(jsdoc(config, cb));
+  var config = require('./jsdocConfig.json')
+  gulp.src(['README.md', './routes/*.js'], {read: false})
+  .pipe(jsdoc(config, cb));
 });
 
-gulp.task('default', ['lint', 'mocha', 'overwrite', 'jsdoc']);
+gulp.task('default', ['lint', 'test', 'overwrite', 'jsdoc']);
