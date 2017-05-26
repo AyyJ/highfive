840ddf63afc611e13023294c53a0a7fd4b97230c
Arthur de Jesus <adejesus@ucsd.edu>
2017-05-26 10:41:00 -0700

    fixes

--- server/app.js
+++ server/app.js
@@ -131,6 +131,12 @@ app.post('/webhook', function (req, res) {
         }
       });
     });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
     res.sendStatus(200);
   }
 });
