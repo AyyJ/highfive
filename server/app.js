3dd2135c883090deb20cd46308478a284d1730f1
Arthur de Jesus <adejesus@ucsd.edu>
2017-05-26 21:24:28 -0700

    test

--- server/app.js
+++ server/app.js
@@ -123,44 +123,35 @@ app.get('/webhook', function(req, res) {
  * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
  *
  */
-app.post('/webhook', function (req, res) {
-  var data = req.body;
+ app.post('/webhook', function (req, res) {
+   var data = req.body;
 
-  // Make sure this is a page subscription
-  if (data.object == 'page') {
-    // Iterate over each entry
-    // There may be multiple if batched
-    data.entry.forEach(function(pageEntry) {
-      var pageID = pageEntry.id;
-      var timeOfEvent = pageEntry.time;
+   // Make sure this is a page subscription
+   if (data.object === 'page') {
 
-      // Iterate over each messaging event
-      pageEntry.messaging.forEach(function(messagingEvent) {
-        if (messagingEvent.optin) {
-          receivedAuthentication(messagingEvent);
-        } else if (messagingEvent.message) {
-          receivedMessage(messagingEvent);
-        } else if (messagingEvent.delivery) {
-          receivedDeliveryConfirmation(messagingEvent);
-        } else if (messagingEvent.postback) {
-          receivedPostback(messagingEvent);
-        } else if (messagingEvent.read) {
-          receivedMessageRead(messagingEvent);
-        } else if (messagingEvent.account_linking) {
-          receivedAccountLink(messagingEvent);
-        } else {
-          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
-        }
-      });
-    });
+     // Iterate over each entry - there may be multiple if batched
+     data.entry.forEach(function(entry) {
+       var pageID = entry.id;
+       var timeOfEvent = entry.time;
 
-    // Assume all went well.
-    //
-    // You must send back a 200, within 20 seconds, to let us know you've
-    // successfully received the callback. Otherwise, the request will time out.
-    res.sendStatus(200);
-  }
-});
+       // Iterate over each messaging event
+       entry.messaging.forEach(function(event) {
+         if (event.message) {
+           receivedMessage(event);
+         } else {
+           console.log("Webhook received unknown event: ", event);
+         }
+       });
+     });
+
+     // Assume all went well.
+     //
+     // You must send back a 200, within 20 seconds, to let us know
+     // you've successfully received the callback. Otherwise, the request
+     // will time out and we will keep trying to resend.
+     res.sendStatus(200);
+   }
+ });
 
 /*
  * Facebook Methods (TODO: REFACTOR TO DIFF FILE)
